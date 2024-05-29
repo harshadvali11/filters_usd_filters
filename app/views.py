f477f431bdcd7ff2cb4399bac9f3316b219fe1e2
jspmern <jspmern@gmail.com>
2024-05-29 16:07:06 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+import datetime
+def filters(request):
+    da=datetime.datetime.now()
+    d={'data':'hai PyTHon How aRe yOU','da':da,'c':10}
+    return render(request,'filters.html',d)
+
+def userfilters(request):
+    d={'data':'hai PyTHon How aRe yOU'}
+    return render(request,'userfilters.html',d)
